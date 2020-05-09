3583cedb3e82a2705645ed8ed456d0e60f37c0e4
Ian Huang <iyh2110@columbia.edu>
2020-05-09 14:05:53 -0400

    moved config-related variables to config.py, and changed read_duration to account for hours-only entries

---
+++ config.py
@@ -0,0 +1,9 @@
+TODOLIST_NAME = 'todo.md'
+NOTES_DIRS = ['test_notes']
+VALID_EXT = ['.md']
+CHECKLIST_CHECKED_MARKERS = ['* [x]'] 
+CHECKLIST_UNCHECKED_MARKERS = ['* [ ]'] 
+HEX_START = 0
+HEX_BITS = 8
+
+     
---
+++ utils.py
@@ -0,0 +1,275 @@
+"""
+utilities functions 
+"""
+import os
+import pickle
+from datetime import datetime
+import config
+
+def ends_in_ext(fname, ext):
+    return fname[-len(ext):] == ext
+
+def ends_in_exts(fname, exts):
+    assert type(exts) is list
+    for ext in exts:
+        if ends_in_ext(fname, ext):
+            return True
+    return False
+
+def has_checklist_marker(line, checked):
+    assert type(line) is str
+    # returns the index where the marker is found and the index corresponding 
+    # to the marker type
+    assert type(config.CHECKLIST_UNCHECKED_MARKERS) is list
+    assert type(config.CHECKLIST_CHECKED_MARKERS) is list
+    if checked:
+        markers_list = config.CHECKLIST_CHECKED_MARKERS
+    else:
+        markers_list = config.CHECKLIST_UNCHECKED_MARKERS
+    for mk_idx, marker in enumerate(markers_list):
+        idx = line.find(marker)
+        if idx != -1:
+            return idx, mk_idx 
+    return None, None
+
+def get_todos(fpath, checked):
+    assert type(fpath) is str
+    assert os.path.exists(fpath)
+    todos = []
+    todo_nb = []
+    with open(fpath, 'r') as f:
+        lines=f.readlines()
+        for l_nb, line in enumerate(lines):
+            idx, mk_idx = has_checklist_marker(line, checked)
+            if idx is not None and mk_idx is not None:
+                todos.append( line[idx:].strip() )
+                todo_nb.append(l_nb)
+
+    return todos, todo_nb
+
+def find_hex(todo):
+    assert type(todo) is str
+    hex_idx = todo.find('{0x')
+    if hex_idx == -1:
+        return None
+    hex_str = todo[hex_idx+len('{0x') : hex_idx+len('{0x')+config.HEX_BITS]
+    try:
+        int(hex_str, 16)
+    except ValueError: 
+        return None
+    
+    return {'where': hex_idx, 'hex': hex_str} 
+
+def generate_hex():
+    # global config.HEX_START
+    new_hex = '0x'+'%0{}X'.format(config.HEX_BITS) % config.HEX_START
+    config.HEX_START += 1
+    return new_hex
+
+def assign_hex(todo, new_hex):
+    # assuming that the todo doesn't already have a hex assigned to it.
+    # if it does, then overwite it
+    old_hex = find_hex(todo)
+    assert type(todo) is str and type(new_hex) is str
+    if old_hex == None:
+        if todo[-1] != ' ':
+            todo += ' '
+        todo += '{' + new_hex + ')'
+    else:
+        todo[old_hex['where'] + len('{') : old_hex['where'] + len('{')+config.HEX_BITS] = new_hex
+    
+    return todo
+
+def read_duration(substring):
+    assert type(substring) is str
+    fragments = substring.split()
+    found = [ (idx, chunk) for idx, chunk in enumerate(fragments) if chunk.isdigit()]
+    if len(found) == 0:
+        return None
+    
+    if len(found)>0 and found[0][0] + 1 < len(fragments):
+        if fragments[found[0][0]+1] == 'm': 
+            return found[0][1] + ' m'
+        if fragments[found[0][0]+1] == 'h':
+            return found[0][1] + ' h'
+    if len(found)>1 and found[1][0] + 1 < len(fragments) and fragments[found[0][0]+1] == 'h':
+        # add hour
+        if fragments[found[1][0]+1] == 'm':
+            return found[0][1] + ' h ' + found[1][1] + ' m'
+        else: # if the other field is filled with some sort of nonsense
+            return found[0][1] + ' h'
+    else:
+        return None
+
+def find_duration(todo):
+    last_comma_idx = todo[::-1].find(',')
+    if last_comma_idx == -1:
+        return None
+    substring = todo[-(last_comma_idx + 1):]
+    import ipdb; ipdb.set_trace()
+    duration = read_duration(substring)
+    if duration == None:
+        print('WARNING: duration missing for {}'.format(todo))
+    
+    return duration
+
+def add_placeholder_duration(todo):
+    
+    last_comma_idx = todo[::-1].find(',')
+    if last_comma_idx == -1:
+        todo += ', 30 m'
+    else:
+        todo = todo[: -(last_comma_idx + 1)] + ' 30 m' + todo[-(last_comma_idx + 1):] 
+    return todo 
+
+def get_new_unchecked_todos(fpath):
+    todos, todo_lnb = get_todos(fpath, False)
+    
+    # check there isn't a hash already given. if so, drop it
+    new_todos = []
+    new_todo_lnb = []
+    for idx, todo in enumerate(todos):
+        if find_hex(todo) is None: # means that it's new
+            new_todos.append(todo)
+            new_todo_lnb.append(todo_lnb[idx])
+
+    new_hashes = [generate_hex() for todo in new_todos]
+    # for each todo, we should check that there's a time duration indicated
+    new_todos_wtime = []
+    for todo in new_todos:
+        duration = find_duration(todo)
+        if duration is None:
+            print('WARNING: adding default duration of 30 m for {}'.format(todo))
+            todo = add_placeholder_duration(todo)
+            new_todos_wtime.append(todo)
+        else:
+            new_todos_wtime.append(todo)    
+    assert len(new_hashes) == len(new_todos_wtime)
+    return list(zip(new_todos_wtime, new_hashes, new_todo_lnb))
+
+def get_checked_hashes(fpath):
+    todos, _ = get_todos(fpath, True)
+    checked_hashes = []
+    for todo in todos:
+        checked_hex = find_hex(todo)
+        if checked_hex is not None: # ignoring adhoc todolists without hashes
+            hex_string = checked_hex['hex']
+            checked_hashes.append(hex_string)
+    return checked_hashes
+
+
+def find_line_with_hash(lines, hex_id):
+    for line_id, line in enumerate(lines):
+        if hex_id in line: 
+            return (line_id, line)
+
+    return None
+
+def check_off_line(line):
+    for idx, element in enumerate(config.CHECKLIST_UNCHECKED_MARKERS):
+        checkbox_idx = line.find(element)
+        if checkbox_idx != -1:
+            return line [:checkbox_idx] + config.CHECKLIST_CHECKED_MARKERS[idx] \
+                    + line[checkbox_idx + len(config.CHECKLIST_UNCHECKED_MARKERS[idx]) : ]
+    
+    for idx, element in enumerate(config.CHECKLIST_CHECKED_MARKERS):
+        checkbox_idx = line.find(element)
+        if checkbox_idx != -1:
+            return line # this line was already checked
+    
+    return None
+
+def check_off_original_notes(fpath, hex_ids):
+    assert type(hex_ids) is list
+    # opens up the file, and then checks off the box
+    try: 
+        with open(fpath, 'r') as f:
+            lines = f.readlines()
+            # find the line with the hex
+            for hex_id in hex_ids:
+                line_tup = find_line_with_hash(lines, hex_id)
+                assert line_tup is not None, "database needs to be updated"
+                line_id = line_tup[0]
+                line = check_off_line(line_tup[1])
+                lines[line_id] = line
+    except FileNotFoundError:
+        print('{} not found'. format(fpath))
+        return False
+
+    try:
+        with open(fpath, 'w') as f:
+            f.writelines(lines) 
+        return True
+    except:
+        return False
+
+def get_date_string(): 
+    date = datetime.now().date()
+    return str(date.month)+'/'+ str(date.day)+'/'+str(date.year)[-2:]
+
+def find_datestring_in_lines(lines, ds):
+    for l_nb, line in enumerate(lines):
+        if ''.join(line.strip().split()) == ds:
+            return l_nb
+    return None 
+
+def find_todoheader_in_lines(lines):
+    return find_datestring_in_lines(lines, '#TODO')
+
+def todoline_formatter(note2newtodos_w_hashes):
+    write_todos = []
+    if len(note2newtodos_w_hashes):
+        for note in note2newtodos_w_hashes:
+            for todo_hex in note2newtodos_w_hashes[note]:
+                write_todos.append ("%s {%s} (in `%s`)"%(todo_hex[0], todo_hex[1], note))
+        write_todos = [element+'\n' for element in write_todos]
+    
+    return write_todos 
+ 
+def write_to_todo(note2newtodos_w_hashes):
+    assert type(note2newtodos_w_hashes) is dict
+    ds =get_date_string()
+    # loading current todo
+    with open(config.TODOLIST_NAME, 'r') as f:
+        lines = f.readlines()
+        d_nb = find_datestring_in_lines(lines, ds)
+    
+    # formatting new todos
+    newlines = todoline_formatter(note2newtodos_w_hashes)
+    if d_nb is None:
+        t_nb = find_todoheader_in_lines(lines)
+        newlines = [ds+'\n'] + newlines + ['\n']
+        new_todolist = lines[:t_nb+1] + newlines + lines[t_nb+1:]
+    else:
+        new_todolist = lines[:d_nb+1] + newlines + lines[d_nb+1:] 
+     
+    # writing current todo 
+    with open(config.TODOLIST_NAME, 'w') as f:
+        f.writelines(new_todolist)
+
+def write_hashes_on_new_todos(note2newtodos_w_hashes):
+    for note in note2newtodos_w_hashes:
+        if len(note2newtodos_w_hashes[note]): # open the file only if you have new todo's from here
+            todo_hashes = note2newtodos_w_hashes[note]
+            # todo_hashes is a list of 3-tuples, where the 3rd element is the line number where the todolist item is
+            with open(note, 'r') as f:
+                lines = f.readlines()
+                for tup in todo_hashes:
+                    # appending hex
+                    lines[tup[2]] = lines[tup[2]].strip() + ' {%s}\n'%tup[1] 
+            with open(note, 'w') as f:
+                f.writelines(lines)
+
+def get_modification_date(filepath):
+    t = os.path.getmtime(filepath)
+    return datetime.fromtimestamp(t)
+
+def has_new_modification(filepath, path2modtime):
+    if filepath not in path2modtime:
+        return True
+    else:
+        mdate = get_modification_date(filepath)
+        assert type(path2modtime[filepath]) is datetime
+        return mdate > path2modtime[filepath]
+
+
