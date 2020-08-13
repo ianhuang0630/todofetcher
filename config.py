a743163d54d601854bdb63d1276669f007021384
Ian Huang <iyh2110@columbia.edu>
2020-08-13 14:48:49 -0400

    added config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TODOLIST_NAME = 'todo.md'
-NOTES_DIRS = ['test_notes']
+NOTES_DIRS = ['travis', 'quadriga', 'papers_queue', 'hack', 'stanford_research']
 VALID_EXT = ['.md']
 CHECKLIST_CHECKED_MARKERS = ['* [x]'] 
 CHECKLIST_UNCHECKED_MARKERS = ['* [ ]'] 
